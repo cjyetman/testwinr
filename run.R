e3f2e4d3f9c2b8c2ef91bb7156ee94b56543fbda
CJ Yetman <cj@cjyetman.com>
2022-01-10 22:40:11 +0100

    Update run.R

--- run.R
+++ run.R
@@ -5,9 +5,9 @@ saveRDS(obj, file = rds_ascii, ascii = TRUE, compress = FALSE)
 file.exists(rds_ascii)
 
 con <- gzfile(rds_ascii, open = "rb")
-on.exit(close(con))
-
 hdr <- readBin(con, what = "raw", n = 5L)
+close(con)
+
 hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
 
 grepl("^RD[ABX][2-9]\n", hdr_char)

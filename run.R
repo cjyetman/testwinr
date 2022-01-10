dfaccb186c22513c1dcca96cc5d2eb7d1bcbe523
CJ Yetman <cj@cjyetman.com>
2022-01-10 21:32:24 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,35 +1,3 @@
-rds_or_rda <- function(filepath) {
-   con <- gzfile(filepath, open = "rb")
-   on.exit(close(con))
-
-   hdr <- readBin(con, what = "raw", n = 5L)
-   hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
-
-   if (grepl("^RD[ABX][2-9]\n", hdr_char)) {
-     return("rda")
-   } else if (grepl("^[AX]\n", hdr_char)) {
-     return("rds")
-   }
-
-    NA_character_
- }
-
 rds_ascii <- tempfile()
 obj <- "xyz"
 saveRDS(obj, file = rds_ascii, ascii = TRUE, compress = FALSE)
-file.exists(rds_ascii)
-
-con <- gzfile(filepath, open = "rb")
-on.exit(close(con))
-
-hdr <- readBin(con, what = "raw", n = 5L)
-hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
-
-if (grepl("^RD[ABX][2-9]\n", hdr_char)) {
-  print("rda")
-} else if (grepl("^[AX]\n", hdr_char)) {
-  print("rds")
-}
-
-print(hdr)
-print(hdr_char)

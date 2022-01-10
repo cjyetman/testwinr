b8a77c69c3a5c42cef30372851bebd30476f2a77
CJ Yetman <cj@cjyetman.com>
2022-01-10 21:00:14 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,30 +1,17 @@
 rds_or_rda <- function(filepaths) {
-   filepaths <- simplify_if_one_col_df(filepaths)
-   stopifnot("`filepaths` must be a character vector" = typeof(filepaths) == "character")
-   filepaths <- canonize_path(filepaths)
+   con <- gzfile(filepath, open = "rb")
+   on.exit(close(con))
 
-   vapply(
-     X = filepaths,
-     FUN = function(filepath) {
-       if (is_file_accessible(filepath)) {
-         con <- gzfile(filepath, open = "rb")
-         on.exit(close(con))
+   hdr <- readBin(con, what = "raw", n = 5L)
+   hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
 
-         hdr <- readBin(con, what = "raw", n = 5L)
-         hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
+   if (grepl("^RD[ABX][2-9]\n", hdr_char)) {
+     return("rda")
+   } else if (grepl("^[AX]\n", hdr_char)) {
+     return("rds")
+   }
 
-         if (grepl("^RD[ABX][2-9]\n", hdr_char)) {
-           return("rda")
-         } else if (grepl("^[AX]\n", hdr_char)) {
-           return("rds")
-         }
-       }
-
-       NA_character_
-     },
-     FUN.VALUE = character(1L),
-     USE.NAMES = FALSE
-   )
+    NA_character_
  }
 
 rds_ascii <- tempfile()

1a0c2b300b4da3de34a0d560a7b58737e2b6fe40
CJ Yetman <cj@cjyetman.com>
2022-01-10 21:53:34 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,3 +1,14 @@
 rds_ascii <- tempfile()
 obj <- "xyz"
 saveRDS(obj, file = rds_ascii, ascii = TRUE, compress = FALSE)
+
+file.exists(rds_ascii)
+
+con <- gzfile(rds_ascii, open = "rb")
+on.exit(close(con))
+
+hdr <- readBin(con, what = "raw", n = 5L)
+hdr_char <- paste(rawToChar(hdr, multiple = TRUE), collapse = "")
+
+grepl("^RD[ABX][2-9]\n", hdr_char)
+grepl("^[AX]\n", hdr_char))
